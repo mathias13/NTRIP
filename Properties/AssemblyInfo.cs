407c51c5b3039dedcc83ca02c1fbe99e8f289a8b
mathias13 <mathias@nolgarden.net>
2014-11-16 10:10:49 +0100

    Changed assembly name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NtripCaster")]
+[assembly: AssemblyTitle("NTRIP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NtripCaster")]
+[assembly: AssemblyProduct("NTRIP")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
