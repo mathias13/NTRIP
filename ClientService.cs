40c08d604563c13270e3d2472afafbb948d88b79
mathias13 <mathias@nolgarden.net>
2017-10-05 21:28:17 +0200

    Fixed bugs not reconnecting

--- ClientService.cs
+++ ClientService.cs
@@ -101,130 +101,136 @@ namespace NTRIP
                     continue;
                 }
 
-                if (_tcpClient == null)
-                    _tcpClient = new TcpClient();
-
-                try
+                lock (_syncObject)
                 {
-                    if (_ipAdress == null)
+                    if (_tcpClient == null)
+                        _tcpClient = new TcpClient();
+
+                    try
                     {
-                        if (!IPAddress.TryParse(_settings.IPorHost, out _ipAdress))
+                        if (_ipAdress == null)
                         {
-                            IPHostEntry ipAdresses;
-                            ipAdresses = Dns.GetHostEntry(_settings.IPorHost);
-                            if (ipAdresses.AddressList.Length < 1)
-                                throw new Exception(String.Format("No valid ip found for: {0}", _settings.IPorHost));
-
-                            _ipAdress = ipAdresses.AddressList[0];
-                        }
-                    }
-                    if (!_tcpClientConnected)
-                        _tcpClient.Connect(_ipAdress, _settings.PortNumber);
-
-                    _tcpClientConnected = true;
-
-                    byte[] authBytes = Encoding.ASCII.GetBytes(String.Format("{0}:{1}", _settings.NTRIPUser.UserName, _settings.NTRIPUser.UserPassword));
-                    string auth = Convert.ToBase64String(authBytes);
-
-                    string msg = String.Format(
-                        "GET /{1} HTTP/1.0{0}"
-                        + "User-Agent: NTRIP NolgardenNTRIPClient/20141010{0}"
-                        + "Accept: */*{0}"
-                        + "Connection: close{0}"
-                        + "Authorization: Basic {2}{0}",
-                            Constants.CRLF, _settings.NTRIPMountPoint, auth);
-
-                    byte[] msgBytes = Encoding.ASCII.GetBytes(msg);
-
-                    _tcpClient.GetStream().Write(msgBytes, 0, msgBytes.Length);
-
-                    DateTime timeOut = DateTime.Now.AddSeconds(30.0);
-                    bool responseComplete = false;
-                    string msgReturned = String.Empty;
-                    while (!responseComplete)
-                    {
-                        if (_tcpClient.Available > 0)
-                        {
-                            byte[] receiveByte = new byte[_tcpClient.Available];
-                            _tcpClient.GetStream().Read(receiveByte, 0, receiveByte.Length);
-                            msgReturned += Encoding.ASCII.GetString(receiveByte);
-                        }
-
-                        if (msgReturned.Contains(Constants.CRLF))
-                        {
-                            if (msgReturned.Contains(Constants.SOURCE200OK))
+                            if (!IPAddress.TryParse(_settings.IPorHost, out _ipAdress))
                             {
-                                if (msgReturned.Contains(Constants.ENDSOURCETABLE))
+                                IPHostEntry ipAdresses;
+                                ipAdresses = Dns.GetHostEntry(_settings.IPorHost);
+                                if (ipAdresses.AddressList.Length < 1)
+                                    throw new Exception(String.Format("No valid ip found for: {0}", _settings.IPorHost));
+
+                                _ipAdress = ipAdresses.AddressList[0];
+                            }
+                        }
+                        if (!_tcpClientConnected)
+                            _tcpClient.Connect(_ipAdress, _settings.PortNumber);
+
+                        NetworkStream stream = _tcpClient.GetStream();
+                        stream.ReadTimeout = 2000;
+                        _tcpClientConnected = true;
+
+                        byte[] authBytes = Encoding.ASCII.GetBytes(String.Format("{0}:{1}", _settings.NTRIPUser.UserName, _settings.NTRIPUser.UserPassword));
+                        string auth = Convert.ToBase64String(authBytes);
+
+                        string msg = String.Format(
+                            "GET /{1} HTTP/1.0{0}"
+                            + "User-Agent: NTRIP NolgardenNTRIPClient/20141010{0}"
+                            + "Accept: */*{0}"
+                            + "Connection: close{0}"
+                            + "Authorization: Basic {2}{0}",
+                                Constants.CRLF, _settings.NTRIPMountPoint, auth);
+
+                        byte[] msgBytes = Encoding.ASCII.GetBytes(msg);
+
+                        _tcpClient.GetStream().Write(msgBytes, 0, msgBytes.Length);
+
+                        DateTime timeOut = DateTime.Now.AddSeconds(30.0);
+                        bool responseComplete = false;
+                        string msgReturned = String.Empty;
+                        while (!responseComplete)
+                        {
+                            if (_tcpClient.Available > 0)
+                            {
+                                byte[] receiveByte = new byte[_tcpClient.Available];
+                                _tcpClient.GetStream().Read(receiveByte, 0, receiveByte.Length);
+                                msgReturned += Encoding.ASCII.GetString(receiveByte);
+                            }
+
+                            if (msgReturned.Contains(Constants.CRLF))
+                            {
+                                if (msgReturned.Contains(Constants.SOURCE200OK))
+                                {
+                                    if (msgReturned.Contains(Constants.ENDSOURCETABLE))
+                                        responseComplete = true;
+                                }
+                                else
                                     responseComplete = true;
                             }
-                            else
-                                responseComplete = true;
+
+                            if (DateTime.Now > timeOut)
+                                break;
                         }
 
-                        if (DateTime.Now > timeOut)
-                            break;
-                    }
-
-                    if (responseComplete)
-                    {
-                        _connect = false;
-                        if (msgReturned.Contains(Constants.ICY200OK))
-                            _serverConnectedAndStreaming = true;
-                        else if (msgReturned.Contains(Constants.UNAUTHORIZED))
+                        if (responseComplete)
                         {
-                            _serverConnectedAndStreaming = false;
-                            OnConnectionException(null, ConnectionFailure.Unauthorized);
-                        }
-                        else if (msgReturned.Contains(Constants.SOURCE200OK))
-                        {
-                            if (_settings.NTRIPMountPoint != String.Empty)
-                                OnConnectionException(null, ConnectionFailure.MountpointNotValid);
-
-                            msgReturned = msgReturned.Replace(Constants.SOURCE200OK + Constants.CRLF, String.Empty);
-                            List<MountPoint> mountPoints = new List<MountPoint>();
-                            string[] mountPointStrings = msgReturned.Split(new string[] { Constants.CRLF }, StringSplitOptions.None);
-                            foreach (string mountPointString in mountPointStrings)
+                            _connect = false;
+                            if (msgReturned.Contains(Constants.ICY200OK))
+                                _serverConnectedAndStreaming = true;
+                            else if (msgReturned.Contains(Constants.UNAUTHORIZED))
                             {
-                                if (mountPointString.StartsWith("STR"))
+                                _serverConnectedAndStreaming = false;
+                                OnConnectionException(null, ConnectionFailure.Unauthorized);
+                            }
+                            else if (msgReturned.Contains(Constants.SOURCE200OK))
+                            {
+                                if (_settings.NTRIPMountPoint != String.Empty)
+                                    OnConnectionException(null, ConnectionFailure.MountpointNotValid);
+
+                                msgReturned = msgReturned.Replace(Constants.SOURCE200OK + Constants.CRLF, String.Empty);
+                                List<MountPoint> mountPoints = new List<MountPoint>();
+                                string[] mountPointStrings = msgReturned.Split(new string[] { Constants.CRLF }, StringSplitOptions.None);
+                                foreach (string mountPointString in mountPointStrings)
                                 {
-                                    string[] fields = mountPointString.Split(new char[] { ';' });
-                                    mountPoints.Add(new MountPoint(fields[1],
-                                                        fields[2],
-                                                        fields[3],
-                                                        (CarrierEnum)Convert.ToInt32(fields[5]),
-                                                        fields[6],
-                                                        Convert.ToSingle(fields[9], new CultureInfo("en-US")),
-                                                        Convert.ToSingle(fields[10], new CultureInfo("en-US"))));
+                                    if (mountPointString.StartsWith("STR"))
+                                    {
+                                        string[] fields = mountPointString.Split(new char[] { ';' });
+                                        mountPoints.Add(new MountPoint(fields[1],
+                                                            fields[2],
+                                                            fields[3],
+                                                            (CarrierEnum)Convert.ToInt32(fields[5]),
+                                                            fields[6],
+                                                            Convert.ToSingle(fields[9], new CultureInfo("en-US")),
+                                                            Convert.ToSingle(fields[10], new CultureInfo("en-US"))));
+                                    }
+
                                 }
 
+                                OnSourceTableReceived(mountPoints.ToArray());
+
+                                _serverConnectedAndStreaming = false;
                             }
-
-                            OnSourceTableReceived(mountPoints.ToArray());
-
-                            _serverConnectedAndStreaming = false;
+                        }
+                        else
+                        {
+                            _connect = false;
+                            OnConnectionException(new Exception("Incomplete or no answer from Caster"), ConnectionFailure.NoAnswer);
                         }
                     }
-                    else
+                    catch (SocketException e)
                     {
                         _connect = false;
-                        OnConnectionException(new Exception("Incomplete or no answer from Caster"), ConnectionFailure.NoAnswer);
+                        OnConnectionException(e, ConnectionFailure.NoInternetConnection);
+                        //Timeout if someone is reconnecting immediatly
+                        Thread.Sleep(1000);
                     }
-                }
-                catch (SocketException e)
-                {
-                    _connect = false;
-                    OnConnectionException(e, ConnectionFailure.NoInternetConnection);
-                    //Timeout if someone is reconnecting immediatly
-                    Thread.Sleep(1000);
-                }
-                catch (Exception e)
-                {
-                    _connect = false;
-                    _tcpClient.Close();
-                    _tcpClientConnected = false;
-                    OnConnectionException(e, ConnectionFailure.UnhandledException);
-                    //Timeout if someone is reconnecting immediatly
-                    Thread.Sleep(1000);
+                    catch (Exception e)
+                    {
+                        _connect = false;
+                        _tcpClient.Close();
+                        _tcpClient = null;
+                        _tcpClientConnected = false;
+                        OnConnectionException(e, ConnectionFailure.UnhandledException);
+                        //Timeout if someone is reconnecting immediatly
+                        Thread.Sleep(1000);
+                    }
                 }
             }
         }
@@ -254,19 +260,21 @@ namespace NTRIP
                             {
                                 if(0 > _tcpClient.Client.Send(new byte[0]))
                                 {
+                                    _serverConnectedAndStreaming = false;
                                     OnConnectionException(new Exception("Lost connection to NTRIP Server"), ConnectionFailure.ConnectionTerminated);
                                     continue;
                                 }
                             }
                             catch (SocketException e)
                             {
+                                _serverConnectedAndStreaming = false;
                                 OnConnectionException(e, ConnectionFailure.ConnectionTerminated);
                                 continue;
                             }
                             aliveCheckTimeout = DateTime.Now.AddSeconds((double)ALIVE_CHECK_TIMEOUT);
                         }
 
-                        byte[] buffer = new byte[1024];
+                        byte[] buffer = new byte[_tcpClient.Available];
                         int bytesReceived = _tcpClient.GetStream().Read(buffer, 0, buffer.Length);
                         if (bytesReceived < 1)
                             Thread.Sleep(1);
@@ -281,6 +289,7 @@ namespace NTRIP
                 }
                 catch (Exception e)
                 {
+                    _serverConnectedAndStreaming = false;
                     OnConnectionException(e, ConnectionFailure.UnhandledException);
                 }
             }
@@ -292,7 +301,7 @@ namespace NTRIP
 
         protected void OnConnectionException(Exception e, ConnectionFailure connectionFailure)
         {
-            _serverConnectedAndStreaming = false;
+            Disconnect();
 
             if (ConnectionExceptionEvent != null)
                 ConnectionExceptionEvent.Invoke(this, new ConnectionExceptionArgs(e, connectionFailure));
